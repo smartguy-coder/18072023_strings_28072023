fd63dcbb18de5d34e6b18b593e8cc0a090b80a92
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-04 20:11:42 +0300

    theory

---
+++ my_numbers.py
@@ -0,0 +1,46 @@
+# user_age_str_format = input('Enter your age: ')
+# # print(type(user_age))
+# user_age = int(user_age_str_format)
+# user_age_float = float(user_age_str_format)
+#
+# converted_from_float_to_int = int(user_age_float)
+#
+# user_age_back_str = str(user_age)
+# print(type(user_age))
+# print(user_age * 2)
+
+# number_int = 4
+# number_float = 4.0
+# strange = 0.1 + 0.2
+#
+# # rounding
+# round_number = round(strange, 4)
+# round_number2 = round(2.5)
+# round_number3 = round(3.5)
+# print(round_number)
+# print(strange)
+
+multiplication = 2 * 2
+multiplication1 = 2 * 2.0
+
+operation = float(multiplication1).is_integer()
+
+division = 2 / 2
+subtraction = 5.0 - 6
+
+powering = 2 ** 3
+powering2 = 25 ** 0.5
+
+integer_division = 17 // 7
+remainder = 17 % 7
+
+is_float_integer = 55.66 % 1
+is_float_integer_correct = not bool(55.0 % 1)
+is_true = bool('0')
+is_true2 = bool('')
+is_true3 = bool(0)
+
+
+
+
+pass
