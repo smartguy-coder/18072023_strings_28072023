dfe91324f9f24238565dd1a8ff4f7f64c43d4193
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-11 18:14:26 +0300

    add f-string example

--- trash.py
+++ trash.py
@@ -5,4 +5,15 @@ ENERGY = 66666
 
 PASSWORD = 'jhfhgjkgkll65656'
 
-user_input = input()
+# user_input = input()
+
+
+data = '11111111111'
+data2 = 22222222
+data3 = 999999999
+data4 = 'kjhkjk'
+
+result = str(data) + ' ' + str(data2) + ' ' + str(data3) + ' ' + str(data4)
+result2 = f'<<{data} {data2} {data3} {data4} {data2 + data3} >>'
+print(result)
+print(result2)
