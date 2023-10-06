9e2f0119f5e962f1d3d8eae441820ea04a83f46b
Василь Картичак <w.i.k.mailua@gmail.com>
2023-10-06 20:14:41 +0300

    mongo4

--- mongo_db.py
+++ mongo_db.py
@@ -27,7 +27,7 @@ vacuum_cleaners_coll = db.vacuum_cleaners
 
 # add many documents ##########################################
 # mops = [
-#     {'_id': 10, 'title': 'Super mop3', 'price': 18},
+#     {'_id': 65656, 'title': 'fffffffff mop3', 'price': 1228},
 #     {'title': 'Super mop4', 'price': 22, 'features': ['speed', 'power']},
 # ]
 # mops_coll.insert_many(mops)
@@ -38,3 +38,24 @@ vacuum_cleaners_coll = db.vacuum_cleaners
 # result = mops_coll.find_one({'_id': 10})  # by field
 # result = mops_coll.find_one({'price': 18})  # by field
 # print(result)
+
+# all data
+# result = mops_coll.find()
+# print(result)
+# for doc in result:
+#       print(doc)
+
+# looking for the specific field
+# query = {'price': 22}
+# query = {'price': {'$gt': 15}}
+# query = {'title': {'$regex': 'Su*'}}
+query = {'title': {'$regex': 'Su*'}}
+
+# result = mops_coll.find(query)
+# result = mops_coll.find(query).limit(2)
+# result = mops_coll.find(query).sort('price').limit(2)
+result = mops_coll.find(query).sort('price', -1).limit(2)
+for doc in result:
+      print(doc)
+
+
