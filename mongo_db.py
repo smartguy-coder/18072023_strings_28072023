fe001f754ff04f2d80a5e8b96067303bffe3e750
Василь Картичак <w.i.k.mailua@gmail.com>
2023-10-06 19:20:08 +0300

    mongo

---
+++ mongo_db.py
@@ -0,0 +1,21 @@
+import pymongo
+
+from config import USER, PASSWORD
+
+url = f'mongodb+srv://{USER}:{PASSWORD}' \
+      '@cluster0.vebb4r3.mongodb.net/?retryWrites=true&w=majority'
+
+client = pymongo.MongoClient(url)
+
+db = client.testDB
+mops_coll = db.mops
+vacuum_cleaners_coll = db.vacuum_cleaners
+
+mops_coll.insert_one(
+      {'title': 'Super mop', 'price': 15}
+)
+
+vacuum_cleaners_coll.insert_one(
+      {'title': 'vacuum cleaner', 'price': 150,
+       'power': 2000}
+)
