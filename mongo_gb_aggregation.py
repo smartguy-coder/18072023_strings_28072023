ea475d9975f85cf2279c02e0d1a402b15dccc15f
Василь Картичак <w.i.k.mailua@gmail.com>
2023-10-13 18:07:35 +0300

    mongo7

--- mongo_gb_aggregation.py
+++ mongo_gb_aggregation.py
@@ -8,4 +8,38 @@ url = f'mongodb+srv://{USER}:{PASSWORD}' \
 client = pymongo.MongoClient(url)
 
 db = client.testDB
-coll = db.mops
+
+collection_products = db.products
+
+products = [
+    {
+        'title': 'bread',
+        'price': 45,
+        'remains': 10,
+        'comment': 'no sugar',
+        'contain_gluten': True,
+    },
+    {
+        'title': 'soft drink',
+        'price': 25,
+        'remains': 100,
+        'comment': 'no sugar',
+        'contain_gluten': False,
+    },
+    {
+        'title': 'milk',
+        'price': 15,
+        'remains': 1,
+        'comment': 'no sugar',
+        'contain_gluten': True,
+    },
+    {
+        'title': 'vinegar',
+        'price': 25,
+        'remains': 10,
+        'comment': 'no sugar',
+        'contain_gluten': False,
+    },
+]
+
+collection_products.insert_many(products)
